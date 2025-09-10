a5ecd1af86032e644ac6a997a880ef00aa71f3d2
bomminenipavani <bomminenipavani555@gmail.c0m>
2025-09-10 23:14:27 +0530

    datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,12 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int value = 10;
+        String data = "java" ;  
+        short bignumber = 10000;
+        System.out.println(".(value)");
+        System.out.println(".(data)");
+        System.out.println(".(bignumber)");
+
+    }
+    
+}
