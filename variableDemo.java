03d482a3ef92a1bbcaad8a7e199bece2a475af40
bomminenipavani <bomminenipavani555@gmail.c0m>
2025-09-11 17:46:25 +0530

    variableDemo.java

---
+++ variableDemo.java
@@ -0,0 +1,13 @@
+public class variableDemo {
+    public static void main(String[] args) {
+        int price = 1000;
+        double rating = 3.5;
+        String brand = "nike";
+
+        System.out.println("price");
+        System.out.println("rating");
+        System.out.println("brand");
+
+    }
+    
+}
