e72210fed5a9e4061877af1e7810d883a72f3603
bomminenipavani <bomminenipavani555@gmail.c0m>
2025-09-23 16:37:03 +0530

    fifth_loops

---
+++ fifth_loops/Demo.java
@@ -0,0 +1,35 @@
+package fifth_loops;
+
+public class Demo {
+    public static void main(String[]args){
+       //without loops
+       //greet
+       System.out.println("Good morning");
+
+       // greet 10n times repeating manual
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+
+        System.out.println("Good morning");
+        System.out.println("Good morning");
+      //with loops
+      //greet 10 times
+      // for loop: used when you know number of Iterations/Repetions in advance
+     For(int count = 1; count <=5;count++){
+         System.out.println(count);
+        }
+     // Forward
+     For (int count = 1; count <=10; ){
+        System.out.println("good morning");
+     }
+     
+
+
+
+    }
+
+
+}
